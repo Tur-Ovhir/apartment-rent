95c10441302abd432a7c772a8d8c7cf85cfe5f31
Tur-Ovhir <ochirtor@gmail.com>
2025-04-15 23:19:53 -0700

    update

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,9 @@
 "use client";
-import { Contact, Done, EmailOption, RentContract } from "@/components/main";
 import LoginPage from "./Login/page";
 export default function Home() {
   return (
-    <div>
-      <div>
+    <div>    
         <LoginPage/>
-       <Done/>
-      </div>
     </div>
   );
 }
