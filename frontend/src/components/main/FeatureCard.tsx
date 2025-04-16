f99c2d67e9366785699b588d7b56580d24f7610b
Tur-Ovhir <ochirtor@gmail.com>
2025-04-15 17:15:09 -0700

    uhaalag

--- frontend/src/components/main/FeatureCard.tsx
+++ frontend/src/components/main/FeatureCard.tsx
@@ -7,8 +7,8 @@ export const FeatureCard = ()=> {
 <div className="flex flex-col m-auto gap-5">
       <div className="flex flex-row gap-4">
         <h1 className="font-bold">Онцлох байр</h1>
-       <Button className="bg-white border text-black -mt-1 hover:bg-gray-300">
-        Ухаалаг эрэмбэ <HiArrowsUpDown className="text-red-500"/>
+       <Button className="bg-white border text-black -mt-1 hover:text-red-500 ">
+        Ухаалаг эрэмбэ <HiArrowsUpDown className="0"/>
        </Button>
       </div>
       <div className="grid grid-cols-4 gap-4 w-full  justify-center mx-auto mt-2">
