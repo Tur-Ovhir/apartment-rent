b3d942a475dbf38fbabe5c79e87456277f083060
Tur-Ovhir <ochirtor@gmail.com>
2025-04-16 18:23:59 -0700

    contentPage

--- frontend/src/app/All/page.tsx
+++ frontend/src/app/All/page.tsx
@@ -1,10 +1,13 @@
 import { FeatureCard, Footer, Navbar, Option } from "@/components/main"
+
 export default function AllPage (){
     return(
         <div className="flex flex-col gap-4">
            <Navbar/>
            <Option/>
+          
            <FeatureCard/>
+          
            <Footer/>
         </div>
     )
--- frontend/src/components/main/LivingRoom.tsx
+++ frontend/src/components/main/LivingRoom.tsx
@@ -3,16 +3,16 @@ import React from "react";
 
 export const LivingRoom = () => {
   return (
-    <div className="flex flex-col md:flex-row p-6 gap-6 bg-white rounded-2xl shadow-lg max-w-7xl mx-auto">
+    <div className="flex flex-col md:flex-row p-6 gap-6 bg-white rounded-2xl shadow-lg max-w-7xl mx-auto border w-[900px] h-[474px]">
       <div className="flex-1">
         <div  className="flex flex-col gap-4">         
           <div className="bg-gray-200 h-64 rounded-lg flex items-end p-4 relative">
             <div className="absolute top-2 left-2 text-xs text-gray-400"></div>          
           </div>
           <div className="flex justify-between gap-2">
-            <div className="w-1/3 h-40 bg-gray-100 rounded" />
             <div className="w-1/3 h-40 bg-gray-200 rounded" />
-            <div className="w-1/3 h-40 bg-gray-100 rounded" />
+            <div className="w-1/3 h-40 bg-gray-200 rounded" />
+            <div className="w-1/3 h-40 bg-gray-200 rounded" />
           </div>
         </div>
       </div>     
