b7debd67cfcbff1240eaef321f73f12bea113db5
Tur-Ovhir <ochirtor@gmail.com>
2025-04-18 00:51:29 -0700

    page name update

--- frontend/src/app/ContentPage/page.tsx
+++ frontend/src/app/ContentPage/page.tsx
@@ -1,16 +1,21 @@
-import { Feature, Footer, GeneralInfo, LivingRoom, MapNotit, Navbar } from "@/components/main";
+import {
+  Feature,
+  Footer,
+  GeneralInfo,
+  LivingRoom,
+  MapNotit,
+  Navbar,
+} from "@/components/main";
 
-export default function ContentPage (){
-    return(
-        <div className="flex flex-col gap-10 justify-center">
-            <Navbar/>
-            <LivingRoom/>           
-            <GeneralInfo/>
-          
-            <Feature/>
-        
-            <MapNotit/>                   
-            <Footer/>
-        </div>
-    )
+export default function ContentPage() {
+  return (
+    <div className="flex flex-col gap-10 justify-center">
+      <Navbar />
+      <LivingRoom />
+      <GeneralInfo />
+      <Feature />
+      <MapNotit />
+      <Footer />
+    </div>
+  );
 }
