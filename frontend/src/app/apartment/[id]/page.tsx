c2e7638d7892882c5d0dc86b20629c85c072d06e
Tur-Ovhir <ochirtor@gmail.com>
2025-04-29 11:10:23 +0800

    uder text update

--- frontend/src/app/apartment/[id]/page.tsx
+++ frontend/src/app/apartment/[id]/page.tsx
@@ -100,8 +100,8 @@ export default function ApartmentIdPage() {
               <div className="space-y-1 mt-2 ">
                 <Dialog>
                   <DialogTrigger asChild>
-                    <Button className="w-40 cursor-pointer text-lg text-black border border-[#434FAA] bg-[#7065F0] ">
-                      Холбогдох
+                    <Button className="w-40 cursor-pointer text-lg text-white border border-[#434FAA] bg-[#7065F0] ">
+                      ХОЛБОГДОХ
                     </Button>
                   </DialogTrigger>
                   <DialogContent>
@@ -120,7 +120,7 @@ export default function ApartmentIdPage() {
                 </Dialog>
                 <Dialog>
                   <DialogTrigger asChild>
-                    <Button className="w-40 cursor-pointer  text-black border  border-[#434FAA] bg-[#7065F0]">
+                    <Button className="w-40 cursor-pointer  text-[#434FAA] border  bg-white border-[#434FAA] ">
                       МЕССЕЖ ИЛГЭЭХ
                     </Button>
                   </DialogTrigger>
@@ -156,7 +156,7 @@ export default function ApartmentIdPage() {
                 </Dialog>
                 <Button
                   onClick={handleCreateContract}
-                  className="w-40 cursor-pointer  border  border-black hover:bg-black  text-white"
+                  className="w-40 cursor-pointer  border  border-black   text-black bg-white"
                 >
                   ГЭРЭЭ БАЙГУУЛАХ
                 </Button>
