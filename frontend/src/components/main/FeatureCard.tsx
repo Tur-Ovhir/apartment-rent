a1d1f03fc7c69887d5f60df1051a9221308619bf
Tur-Ovhir <ochirtor@gmail.com>
2025-05-02 11:22:27 +0800

    next error update

--- frontend/src/components/main/FeatureCard.tsx
+++ frontend/src/components/main/FeatureCard.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import { HiArrowsUpDown } from "react-icons/hi2";
 import { OneCard } from "./OneCard";
@@ -8,19 +8,26 @@ import { ApartmentType } from "@/types";
 interface FeatureCardProps {
   apartments: ApartmentType[];
 }
+
 export const FeatureCard = ({ apartments }: FeatureCardProps) => {
-  const isHighlight = localStorage.getItem("isHighlight");
+  const [isHighlight, setIsHighlight] = useState("0");
+
+  useEffect(() => {
+    const highlight = localStorage.getItem("isHighlight") || "0";
+    setIsHighlight(highlight);
+  }, []);
+
   return (
     <div className="flex flex-col mt-6 gap-5 w-full">
       <div className="flex flex-row gap-4 w-full">
         <h1 className="font-bold text-2xl">
-          {isHighlight == "1" ? "Онцлох байр" : "Сүүлд нэмэгдсэн байр"}
+          {isHighlight === "1" ? "Онцлох байр" : "Сүүлд нэмэгдсэн байр"}
         </h1>
-        <Button className="bg-white border text-black hover:text-red-500  cursor-pointer">
-          Ухаалаг эрэмбэ <HiArrowsUpDown className="0" />
+        <Button className="bg-white border text-black hover:text-red-500 cursor-pointer">
+          Ухаалаг эрэмбэ <HiArrowsUpDown />
         </Button>
       </div>
-      <div className="grid grid-cols-4 gap-4 w-full  justify-center mx-auto mt-2">
+      <div className="grid grid-cols-4 gap-4 w-full justify-center mx-auto mt-2">
         {apartments.map((apt: ApartmentType) => (
           <OneCard
             key={apt.id}
